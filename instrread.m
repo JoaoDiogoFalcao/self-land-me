ff8d238852084eaef7d703ebc97d596a8cea79dc
Joao Diogo Falcao <john.diogo.m.falcao@gmail.com>
2015-12-06 21:59:57 -0800

    Callback for Serial Communicatin

--- instrread.m
+++ instrread.m
@@ -1,4 +1,4 @@
-function instrread(s, event, mode, cur_cols, cur_rows, ls)
+function instrread(s, event, mode, cur_cols, cur_rows,cur_lat, cur_lng,  cur_heading, ls1, ls2, MasterMatrix, Origin)
     if(mode==1 && s.BytesAvailable > 22) % Header
         cur_altitude = fread(s, 1, 'uint8');
         cur_heading = fread(s, 1, 'uint16');
@@ -17,7 +17,7 @@ function instrread(s, event, mode, cur_cols, cur_rows, ls)
         %    cur_altitude,cur_heading,cur_rows,cur_cols, ceil((double(cur_rows)*double(cur_cols))/8),cur_lat,cur_lng);
         
         % Ready for the Map
-        s.BytesAvailableFcn = {'instrread', 2,cur_cols,cur_rows, ls}; % 2 for Map
+        s.BytesAvailableFcn = {'instrread', 2,cur_cols,cur_rows,cur_lat, cur_lng, cur_heading, ls1, ls2, MasterMatrix, Origin}; % 2 for Map
     else
         %fprintf('Bytes Available: %i, ColsxRows: %i x %i = %i\n', s.BytesAvailable, cur_cols, cur_rows,cur_cols*cur_rows);
         if(s.BytesAvailable > ((cur_cols*cur_rows)/8)-1)
@@ -35,10 +35,12 @@ function instrread(s, event, mode, cur_cols, cur_rows, ls)
               end
             end 
             map = vec2mat(map_n, cur_cols)';
-            delete(ls);
-            ls = surf(map);
-            pause(0.000001);
-            s.BytesAvailableFcn = {'instrread', 1,80,20,ls}; % 1 for New Header
+            %delete(ls1);
+            %ls = surf(map);
+            %set(ls1,'ZData', map);
+            %pause(0.0001);
+            updateMasterMatrix(map, cur_heading, cur_cols, cur_rows, [cur_lat cur_lng], [1 1], ls1, ls2, MasterMatrix, Origin);
+            s.BytesAvailableFcn = {'instrread', 1,80,20,cur_lat, cur_lng,cur_heading, ls1, ls2, MasterMatrix, Origin}; % 1 for New Header
         end
     end
     
